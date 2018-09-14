bce9f310594879d42e2746633e4fbdc756b52eb3
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2018-09-14 10:39:15 +0200

    fixes spec

--- spec/pretty_text_spec.rb
+++ spec/pretty_text_spec.rb
@@ -10,7 +10,7 @@ describe PrettyText do
       MD
 
       html = <<~HTML
-        <p><span class="chcklst-box fa fa-square-o"></span>,<span class="chcklst-box fa fa-square-o"></span>,<span class="chcklst-box fa fa-square"></span>;<span class="chcklst-box fa fa-minus-square-o"></span>X<span class="chcklst-box checked fa fa-check-square"></span>X <span class="chcklst-box checked fa fa-check-square-o"></span> <span class="chcklst-box checked fa fa-check-square-o"></span> are all checkboxes<br>
+        <p><span class="chcklst-box fa fa-square-o fa-fw"></span>,<span class="chcklst-box fa fa-square-o fa-fw"></span>,<span class="chcklst-box fa fa-square fa-fw"></span>;<span class="chcklst-box fa fa-minus-square-o fa-fw"></span>X<span class="chcklst-box checked fa fa-check-square fa-fw"></span>X <span class="chcklst-box checked fa fa-check-square-o fa-fw"></span> <span class="chcklst-box checked fa fa-check-square-o fa-fw"></span> are all checkboxes<br>
         <code>[ ]</code> <a>x</a> <em>[ ]</em> <strong>[ ]</strong> are not checkboxes</p>
       HTML
       cooked = PrettyText.cook(md)
